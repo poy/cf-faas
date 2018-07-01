21a966352abe4759387226233688853d44332504
Andrew Poydence <apoydence@pivotal.io>
2018-07-01 07:22:48 -0600

    Improve install.sh logging

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,35 +28,46 @@ fi
 
 TEMP_DIR=$(mktemp -d)
 
-GOOS=linux go build -o $TEMP_DIR/cf-faas ../cmd/cf-faas
-GOOS=linux go build -o $TEMP_DIR/task-runner ../cmd/task-runner
-GOOS=linux go build -o $TEMP_DIR/worker ../cmd/worker
+# CF-FaaS binaries
+echo "building CF-FaaS binaries..."
+GOOS=linux go build -o $TEMP_DIR/cf-faas ../cmd/cf-faas &> /dev/null || echo "failed to build cf-faas"
+GOOS=linux go build -o $TEMP_DIR/task-runner ../cmd/task-runner &> /dev/null || echo "failed to build cf-faas' task-runner"
+GOOS=linux go build -o $TEMP_DIR/worker ../cmd/worker &> /dev/null || echo "failed to build cf-faas' worker"
 cp ../cmd/cf-faas/run.sh $TEMP_DIR
+echo "done building CF-FaaS binaries."
 
-# CF-Space-Security
-go get github.com/apoydence/cf-space-security/...
-GOOS=linux go build -o $TEMP_DIR/proxy ../../cf-space-security/cmd/proxy
-GOOS=linux go build -o $TEMP_DIR/reverse-proxy ../../cf-space-security/cmd/reverse-proxy
+# CF-Space-Security binaries
+echo "building CF-Space-Security binaries..."
+go get github.com/apoydence/cf-space-security/... &> /dev/null || echo "failed to get cf-space-security"
+GOOS=linux go build -o $TEMP_DIR/proxy ../../cf-space-security/cmd/proxy &> /dev/null || echo "failed to build cf-space-security proxy"
+GOOS=linux go build -o $TEMP_DIR/reverse-proxy ../../cf-space-security/cmd/reverse-proxy &> /dev/null || echo "failed to build cf-space-security reverse proxy"
+echo "done building CF-Space-Security binaries."
 
-cf push $app_name --no-start -p $TEMP_DIR -b binary_buildpack -c ./run.sh
+echo "pushing $app_name..."
+cf push $app_name --no-start -p $TEMP_DIR -b binary_buildpack -c ./run.sh &> /dev/null || echo "failed to push app $app_name"
+echo "done pushing $app_name."
 
 if [ -z ${CF_HOME+x} ]; then
     CF_HOME=$HOME
 fi
 
+# Configure
+echo "configuring $app_name..."
+cf set-env $app_name REFRESH_TOKEN "$(cat $CF_HOME/.cf/config.json | jq -r .RefreshToken)" &> /dev/null || echo "failed to set REFRESH_TOKEN"
+cf set-env $app_name CLIENT_ID "$(cat $CF_HOME/.cf/config.json | jq -r .UAAOAuthClient)" &> /dev/null || echo "failed to set set CLIENT_ID"
+cf set-env $app_name MANIFEST "$(cat $manifest_path)" &> /dev/null || echo "failed to set MANIFEST"
+cf set-env $app_name PLUGIN_URLS "$plugins" &> /dev/null || echo "failed to set PLUGIN_URLS"
+
 skip_ssl_validation="$(cat $CF_HOME/.cf/config.json | jq -r .SSLDisabled)"
-
-cf set-env $app_name REFRESH_TOKEN "$(cat $CF_HOME/.cf/config.json | jq -r .RefreshToken)"
-cf set-env $app_name CLIENT_ID "$(cat $CF_HOME/.cf/config.json | jq -r .UAAOAuthClient)"
-cf set-env $app_name MANIFEST "$(cat $manifest_path)"
-cf set-env $app_name PLUGIN_URLS "$plugins"
-
 if [ $skip_ssl_validation = "true" ]; then
-    cf set-env $app_name SKIP_SSL_VALIDATION true
+    cf set-env $app_name SKIP_SSL_VALIDATION true &> /dev/null || echo "failed to set SKIP_SSL_VALIDATION"
 fi
 
 if [ ! -z "$bootstrap_path" ]; then
-    cf set-env $app_name BOOTSTRAP_MANIFEST "$(cat $bootstrap_path)"
+    cf set-env $app_name BOOTSTRAP_MANIFEST "$(cat $bootstrap_path)" &> /dev/null || echo "failed to set BOOTSTRAP_MANIFEST"
 fi
+echo "done configuring $app_name."
 
-cf start $app_name
+echo "starting $app_name..."
+cf start $app_name &> /dev/null || echo "failed to start $app_name"
+echo "done starting $app_name."

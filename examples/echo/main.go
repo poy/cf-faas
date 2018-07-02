68dd0b9649930e20ffb3e34875449d367552027f
Andrew Poydence <apoydence@pivotal.io>
2018-07-02 15:56:42 -0600

    Adds echo example

---
+++ examples/echo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import faas "github.com/apoydence/cf-faas"
+
+func main() {
+	faas.Start(faas.HandlerFunc(func(req faas.Request) (faas.Response, error) {
+		return faas.Response{
+			StatusCode: 200,
+			Body:       req.Body,
+		}, nil
+	}))
+}
